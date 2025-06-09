ec1f7739472d5f23e50593f954ac4f54cc2f53da
Bartek <baarti02@gmail.com>
2025-06-09 15:49:04 +0200

    Updated other files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module com.example.currencycalculator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires org.json;
+    requires java.sql;
 
 
     opens com.example.currencycalculator to javafx.fxml;
     exports com.example.currencycalculator;
+    exports com.example.currencycalculator.controller;
+    opens com.example.currencycalculator.controller to javafx.fxml;
 }
